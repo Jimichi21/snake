32f2cdbeb1efeb2408ba0a6be415273b79b2ce6d
Agustín <Agustn@192.168.1.42>
2017-05-27 18:18:30 +0200

    Primer test hecho

--- Protocolos-Juegos/Snake/src/test/java/es/codeurjc/em/snake/SnakeTest.java
+++ Protocolos-Juegos/Snake/src/test/java/es/codeurjc/em/snake/SnakeTest.java
@@ -2,99 +2,97 @@ package es.codeurjc.em.snake;
 
 import static org.junit.Assert.assertTrue;
 
-import java.util.concurrent.atomic.AtomicReference;
+import java.util.concurrent.atomic.AtomicReferenceArray;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
 import java.util.concurrent.*;
 public class SnakeTest {
 
-	@BeforeClass
-	public static void startServer(){
-		Application.main(new String[]{ "--server.port=8080" });
-	}
-		
-	@Test
-	public void testConnection() throws Exception {
-		
-		WebSocketClient wsc = new WebSocketClient();
-		wsc.connect("ws://127.0.0.1:8080/snake");
-        wsc.disconnect();		
-	}
-	volatile int i=0;
-	@Test
-	public void testJoin() throws Exception {
-		
-		
-		  Executor executor = Executors.newFixedThreadPool(4);
-		
-		AtomicReference<String> firstMsg = new AtomicReference<String>();
-		
-		
-		Runnable tarea=()->{
+ @BeforeClass
+ public static void startServer(){
+  Application.main(new String[]{ "--server.port=8080" });
+ }
+  
+ @Test
+ public void testConnection() throws Exception {
+  
+  WebSocketClient wsc = new WebSocketClient();
+  wsc.connect("ws://127.0.0.1:8080/snake");
+        wsc.disconnect();  
+ }
+ volatile int i=0;
+ @Test
+ public void testJoin() throws Exception {
+  
+  
+    Executor executor = Executors.newFixedThreadPool(4);
+  
+  AtomicReferenceArray<String> firstMsg = new AtomicReferenceArray<String>(4);
+  
+  
+  Runnable tarea=()->{
+
+int id=Character.getNumericValue(Thread.currentThread().getName().charAt(Thread.currentThread().getName().length()-1)-1);
+	  
+	  
 WebSocketClient wsc = new WebSocketClient();
-			
+   
 wsc.onMessage((session, msg) -> {
-	System.out.println("TestMessage: "+msg);
-	firstMsg.compareAndSet(null, msg);
+	
+ System.out.println("TestMessage: "+msg);
+if(msg.contains("update")){
+ firstMsg.compareAndSet(id,null, msg);
+}
 });
 
-			try{
-			wsc.connect("ws://127.0.0.1:8080/snake");
-			
-		        
-		        System.out.println("Connected");
-			
-			String msg;
-			System.out.println("Nombre "+Thread.currentThread().getName());
-			if(i==0){
-		     msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Crear\",\"Sala\":\"1\"}", "Creador");
-		        i++;
-	        }else{
-	       Thread.sleep(500);
-	        msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Unir\",\"Sala\":\"1\"}", "Union");
-	        }
-			wsc.sendMessage(msg);
-			
-			Thread.sleep(10000);
-			wsc.disconnect();	
-			
-			}catch(Exception e){
-				e.printStackTrace();
-			}
-		
-		};
-		
-		
-		
-		executor.execute(tarea);
-	Thread.sleep(500);
-		executor.execute(tarea);
-		//Thread.sleep(1000);
-		executor.execute(tarea);
-	//Thread.sleep(1000);
-		executor.execute(tarea);
-		
-		
-		Thread.sleep(15000);
-		
-		
-		
-		
-		
-		/*
-		if(nombre.equals("Bruce Lee")){
-	        
-	        msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Crear\",\"Sala\":\"1\"}", nombre);
-	        
-	        }else{
-	       Thread.sleep(500);
-	        msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Unir\",\"Sala\":\"1\"}", nombre);
-	        }
-			wsc.sendMessage(msg);*/
-		
-        	
-	}
-
+   try{
+   wsc.connect("ws://127.0.0.1:8080/snake");
+   
+          
+          System.out.println("Connected");
+   
+   String msg;
+   System.out.println("Nombre "+Thread.currentThread().getName());
+   if(i==0){
+       msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Crear\",\"Sala\":\"1\"}", "Creador");
+          i++;
+         }else{
+       
+         msg=String.format("{\"type\": \"user\", \"user\": \"%s\", \"ComandoSala\":\"Unir\",\"Sala\":\"1\"}", "Union");
+         }
+   wsc.sendMessage(msg);
+   
+   Thread.sleep(1500);
+   wsc.disconnect(); 
+   
+   }catch(Exception e){
+    e.printStackTrace();
+   }
+  
+  };
+  
+  
+  
+  
+  
+  
+  executor.execute(tarea);
+ Thread.sleep(500);
+  executor.execute(tarea);
+  //Thread.sleep(1000);
+  executor.execute(tarea);
+ //Thread.sleep(1000);
+  executor.execute(tarea);
+  
+  
+  Thread.sleep(2500);
+  
+  for(int h=0;h<4;h++){
+	  
+	  String msg=firstMsg.get(h);
+	  assertTrue("The first message should contain 'join', but it is "+msg, msg.contains("update"));
+  }
+  
+ }
 }
-
